aaf6de1ec25e4f81741102c9b3fcead1295cdf18
mobrien123 <m.obrien@audionetwork.com>
2020-07-15 15:30:04 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("When running the app", () => {
+  const wrapper = shallow(<App />);
+
+  it("should render the app", () => {
+    expect(wrapper.length).toBe(1);
+  });
+  
 });
+
+
