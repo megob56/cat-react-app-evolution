1eb1f4b60edf9a4d477c47a373c4d695c10f402d
mobrien123 <m.obrien@audionetwork.com>
2020-07-15 17:04:14 +0100

    attempt#1

--- src/App.js
+++ src/App.js
@@ -4,16 +4,45 @@ import Modal from 'react-modal';
 import './App.css';
 
 class App extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state({
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      isModalOpen: false,
+    };
+  }
 
-  //   })
-  // }
+  openModal = () => {
+    this.setState({
+      isModalOpen: true
+    });
+  }
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false
+    });
+  }
+
+
   render(){
+    const owners = ["Dan G", "Jame'z", "Sam", "Stjep", "Chris", "Rob", "JB", "Shuks"];
     return(
       <div className="App">
-        <h1>Heyyy There</h1>
+        <h1>Owners</h1>
+        <table className="js-owner-table">
+          <tbody>
+            {owners.map(owner => (
+              <tr key={`row-of-${owner}`}>
+                <td key={owner}>{owner}</td>
+                <button className={"js-open-modal-button"} onClick={ this.openModal }>Add cat</button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Modal isOpen={ this.state.isModalOpen }>
+          <button className="js-close-modal-button" onClick={ this.closeModal }></button>
+        </Modal>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,42 @@ import App from "./App";
 
 describe("When running the app", () => {
   const wrapper = shallow(<App />);
+  const owners = ["Dan G", "Jame'z", "Sam", "Stjep", "Chris", "Rob", "JB", "Shuks"];
+  const ownerButtons = owners.map(name => ".js-modal-button-for-"+name);
 
   it("should render the app", () => {
     expect(wrapper.length).toBe(1);
   });
+
+  it("should display a table of owners", () => {
+    expect(wrapper.find(".js-owner-table").length).toBe(1);
+  });
+
+  it("should display a table with a row named after the owner & a button", () => {
+    expect(wrapper.find(".js-open-modal-button").length).toBe(owners.length);
+    //ownerButtons.forEach(owner => expect(wrapper.find(owner).length).toBe(1));
   
+  });
+
+  it("should not display the modal", () => {
+    expect(wrapper.state().isModalOpen).toBe(false);
+  });
+
+  describe("When opening the modal", () => {
+    beforeAll(() => {
+      wrapper.find('.js-open-modal-button').simulate('click');
+    });
+    
+    it("the modal should be visible", () => {
+      expect(wrapper.state().isModalOpen).toBe(true);
+      expect(wrapper.find('.js-add-cat-to-owner-modal').length).toBe(1);
+    });
+
+    it("the modal should display a close button", () => {
+      expect(wrapper.find(".js-close-modal-button").length).toBe(1);
+    });
+  });
+
 });
 
 
