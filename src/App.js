a0c2a6971f3b42ffe291a97abe9c03106ad57e4d
mobrien123 <m.obrien@audionetwork.com>
2020-07-16 11:23:21 +0100

    refactored to add additional tests

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends React.Component {
     
     this.state = {
       isModalOpen: false,
+      breeds: [],
+      selectedBreed: "",
+
     };
   }
 
@@ -24,6 +27,23 @@ class App extends React.Component {
     });
   }
 
+  handleChange = (e) => {
+    this.setState({ selectedBreed: e.target.value })
+  }
+
+  componentDidMount() {
+    let initialBreeds = [];
+    fetch("https://api.thecatapi.com/v1/breeds")
+      .then((response) => response.json())
+      .then((data => {
+        initialBreeds = data.map((x) => {
+          return x.name
+        });
+
+        this.setState({breeds: initialBreeds});
+    }));
+  }
+
 
   render(){
     const owners = ["Dan G", "Jame'z", "Sam", "Stjep", "Chris", "Rob", "JB", "Shuks"];
@@ -34,15 +54,25 @@ class App extends React.Component {
           <tbody>
             {owners.map(owner => (
               <tr key={`row-of-${owner}`}>
-                <td key={owner}>{owner}</td>
+                <td key={owner} className="js-owner-name-row">{owner}{this.state.selectedBreed}</td>
                 <button className={"js-open-modal-button"} onClick={ this.openModal }>Add cat</button>
               </tr>
             ))}
           </tbody>
         </table>
-        <Modal isOpen={ this.state.isModalOpen }>
+        <Modal isOpen={ this.state.isModalOpen } className="js-add-cat-to-owner-modal">
           <button className="js-close-modal-button" onClick={ this.closeModal }>Close</button>
-          <button className="js-add-cat-to-owner-modal">Add cat</button>
+          <div className = "js-select-menu-div">
+            <select className='js-select-cat-breed-menu' onChange={ this.handleChange } value = { this.state.selectedBreed }>
+                  <option>Choose One...</option>
+                  {this.state.breeds.map(breed => (
+                      <option key={breed} value={breed}>
+                          {breed}
+                      </option>
+                  ))}
+            </select>
+          </div>
+          <button className="js-add-cat-to-owner-button">Add</button>
         </Modal>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,35 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
+global.fetch = jest.fn(() => "default")
+  .mockImplementationOnce(() => Promise.resolve({
+		json: () =>
+			Promise.resolve([
+				{
+          name: "catBreed",
+				},
+			]),
+  }))
+  .mockImplementationOnce(() => Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          id: "breedID",
+        },
+      ]),
+  })) 
+  .mockImplementationOnce(() => Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          url: "catImageURL",
+        },
+      ]),
+  }));
+
 describe("When running the app", () => {
   const wrapper = shallow(<App />);
   const owners = ["Dan G", "Jame'z", "Sam", "Stjep", "Chris", "Rob", "JB", "Shuks"];
-  // const ownerButtons = owners.map(name => ".js-modal-button-for-"+name);
 
   it("should render the app", () => {
     expect(wrapper.length).toBe(1);
@@ -17,9 +42,6 @@ describe("When running the app", () => {
 
   it("should display a table with a row named after the owner & a button", () => {
     expect(wrapper.find(".js-open-modal-button").length).toBe(owners.length);
-    //ownerButtons.forEach(owner => expect(wrapper.find(owner).length).toBe(1));
-    console.log(wrapper.find(".js-open-modal-button"))
-  
   });
 
   it("should not display the modal", () => {
@@ -27,14 +49,8 @@ describe("When running the app", () => {
   });
 
   describe("When opening the modal", () => {
-    const ownerButtons = Array.from(wrapper.find('.js-open-modal-button'));
-    console.log(ownerButtons);
-
     beforeAll(() => {
       wrapper.find('.js-open-modal-button').at(0).simulate('click');
-      // const ownerButtons = owners.map(name => ".js-modal-button-for-"+name);
-      //ownerButtons.forEach(x => x.prop('onClick')());
-
     });
 
     it("the modal should be visible", () => {
@@ -42,8 +58,55 @@ describe("When running the app", () => {
       expect(wrapper.find('.js-add-cat-to-owner-modal').length).toBe(1);
     });
 
-    it("the modal should display a close button", () => {
+    it("the modal should display a close button & add cat button", () => {
       expect(wrapper.find(".js-close-modal-button").length).toBe(1);
+      expect(wrapper.find(".js-add-cat-to-owner-button").length).toBe(1);
+    });
+
+    it("should make an api call", () => {
+      expect(fetch).toHaveBeenCalledWith("https://api.thecatapi.com/v1/breeds");
+    })
+
+    it("should display a drop down list", () => {
+      expect(wrapper.find('.js-select-cat-breed-menu').length).toBe(1);
+    });
+
+    describe("When choosing a cat breed from the drop down list", () => {
+      beforeAll(() => {
+        wrapper.setState({selectedBreed: "test"});
+        wrapper.setState({isModalOpen: true});
+      });
+
+      it("should call the api", () => {
+        expect(fetch).toHaveBeenCalledWith(`https://api.thecatapi.com/v1/breeds/search?q=test`);
+        expect(wrapper.state().breedId).toBe("breedID");
+        expect(fetch).toHaveBeenCalledWith(`https://api.thecatapi.com/v1/images/search?breed_ids=breedID`);
+      });
+
+      it("should render all images of the selected breed in the modal", () => {
+        expect(wrapper.state().catImage).toBe("catImageURL");
+      });
+    });
+  });
+
+  describe("When clicking the add cat button", () => {
+    beforeAll(() => {
+      wrapper.setState({isModalOpen: true});
+      wrapper.setState({selectedBreed: "American Shorthair"});
+      wrapper.find('.js-add-cat-to-owner-button').simulate("click");
+    });
+    it("should add the name of the cat next to the owners name in the track row", () => {
+      expect(wrapper.find(".js-owner-name-row").at(0).text()).toBe("Dan G - American Shorthair");
+    });
+  });
+
+  describe("When clicking the close modal button after adding a cat", () => {
+    beforeAll(() => {
+      wrapper.setState({isModalOpen: true});
+      wrapper.find('.js-close-modal-button').simulate("click");
+    })
+    it("should no longer display the modal", () => {
+      expect(wrapper.state().isModalOpen).toBe(false);
     });
   });
 
