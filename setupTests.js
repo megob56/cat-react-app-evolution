81822594bdc0ac24bac0a2d18757f9339ae6211b
mobrien123 <m.obrien@audionetwork.com>
2020-07-22 10:30:38 +0100

    npm eject from create react app space, attempt to fix failed test api call

--- setupTests.js
+++ setupTests.js
@@ -7,7 +7,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import fetchMock from "jest-fetch-mock";
 
-fetchMock.enableMocks();
+//fetchMock.enableMocks();
 global.fetch = fetchMock;
 
 configure({ adapter: new Adapter() });
